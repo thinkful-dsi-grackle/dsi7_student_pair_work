58663de4380571a05aba78685fed286c5cf5e64d
Dominic Lindog <67496237+demicosta@users.noreply.github.com>
2020-09-14 11:09:32 -0700

    added SQL queries of Caesar and Lucnalie

---
+++ week02_sql/fueleconomy_queries_Caesar_Lucnalie.sql
@@ -0,0 +1,6 @@
+SELECT make, model, year, cty AS city_mpg,
+CASE WHEN cty < 18 THEN 'Below Average'	
+ELSE 'Above Average' END AS fuel_efficiency
+FROM vehicles
+WHERE year BETWEEN 1990 AND 2000 AND make = 'Ford'
+LIMIT 100;
